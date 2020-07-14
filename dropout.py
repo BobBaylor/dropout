fbc2dca8f47a6787cee4ea4af5ac250078a31763
Bob Baylor <SkiBoyBob@gmail.com>
2020-07-13 17:57:45 -0700

    lint

--- dropout.py
+++ dropout.py
@@ -1,9 +1,14 @@
-
+""" opens a socket to a DNS server.
+    When it fails, it prints a timestamp and a description of the failure.
+"""
 import socket
 import time
 from random import random
 
 def socket_to_host(host="8.8.8.8", port=53, timeout=3):
+    """ Attempt to open a socket.
+        Return bool for success and a string of info.
+    """
     try:
         socket.setdefaulttimeout(timeout)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
@@ -12,28 +17,28 @@ def socket_to_host(host="8.8.8.8", port=53, timeout=3):
         return False, f'{ex}'
 
 def str_now():
+    """ Return a timestamp string
+    """
     return time.strftime('%b-%d %H:%M:%S', time.localtime())
 
-if __name__ == '__main__':
-    host_ips = [
-        '8.8.8.8',
-        '208.67.222.222',
-        '208.67.220.220',
-        # '192.168.2.3',
-        ]
-    ticker = '|/-\\'
-    print(f'starting at {str_now()}')
-    i = 0
-    j = 0
-    while True:
-        i = i+1
-        i = i % len(host_ips)
-        j = j+1
-        j = j % len(ticker)
-        b_good, e_str = socket_to_host(host_ips[i])
-        if not b_good:
-            print(f'FAILED at {str_now()} to {host_ips[i]} {e_str}')
-        else:
-            print(ticker[j]+'\r', end=' ', flush=True)
-        time.sleep(2 + random() * 2)
+HOST_IPS = [
+    '8.8.8.8',          # google DNS server
+    '208.67.222.222',   # open DNS
+    '208.67.220.220',   # open DNS
+    # '192.168.2.3',    # test IP that I know will not respond
+    ]
 
+TICKER = '|/-\\'
+
+if __name__ == '__main__':
+    print(f'{__file__} starting at {str_now()}')
+    i, j = 0, 0
+    while True:
+        i, j = i+1, j+1
+        i, j = i % len(HOST_IPS), j % len(TICKER)
+        b_good, e_str = socket_to_host(HOST_IPS[i])
+        if not b_good:
+            print(f'FAILED at {str_now()} to {HOST_IPS[i]} {e_str}')
+        else:
+            print(TICKER[j]+'\r', end=' ', flush=True)
+        time.sleep(2 + random() * 2)
